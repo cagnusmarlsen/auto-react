9cdb590a798d3d6f526efd8de8c13ec38c3a3f11
cagnusmarlsen <0arunabh30@gmail.com>
2024-09-03 23:37:42 +0530

    save posts and track used accounts

--- main.py
+++ main.py
@@ -21,31 +21,46 @@ def sign_in(driver, username, password):
     time.sleep(3)
 
 def updoot(driver, blog_url):
-    driver.get(blog_url)
-    time.sleep(3)
+    try:
+        driver.get(blog_url)
+        time.sleep(3)
+        
+        # Randomly scrolls the page
+        scroll_height = driver.execute_script("return document.body.scrollHeight")
+        for i in range(5):
+            driver.execute_script(f"window.scrollTo(0, {scroll_height * random.uniform(0.2, 0.8)});")
+            time.sleep(random.uniform(0.5, 2))
+
+        # Locate and click on all reactions
+        time.sleep(0.5)
+        like_button = driver.find_element(By.XPATH, "//*[@id='reaction-drawer-trigger']")
+        like_button.click()
+
+        time.sleep(0.5)
+        unicorn_button = driver.find_element(By.XPATH, "//*[@id='reaction-butt-unicorn']")
+        unicorn_button.click()
+
+        time.sleep(0.5)
+        exploding_head = driver.find_element(By.XPATH, "//*[@id='reaction-butt-exploding_head']")
+        exploding_head.click()
+
+        time.sleep(0.5)
+        raised_hands = driver.find_element(By.XPATH, "//*[@id='reaction-butt-raised_hands']")
+        raised_hands.click()
+
+        time.sleep(0.5)
+        fire = driver.find_element(By.XPATH, "//*[@id='reaction-butt-fire']")
+        fire.click()
+
+        time.sleep(2)
+        save_button = driver.find_element(By.XPATH, "//*[@id='reaction-butt-readinglist']")
+        save_button.click()
+
+        return True
     
-    # Randomly scrolls the page
-    scroll_height = driver.execute_script("return document.body.scrollHeight")
-    for i in range(5):
-        driver.execute_script(f"window.scrollTo(0, {scroll_height * random.uniform(0.2, 0.8)});")
-        time.sleep(random.uniform(0.5, 2))
-
-    # Locate and click on all reactions
-    like_button = driver.find_element(By.XPATH, "//*[@id='reaction-drawer-trigger']")
-    like_button.click()
-
-    unicorn_button = driver.find_element(By.XPATH, "//*[@id='reaction-butt-unicorn']")
-    unicorn_button.click()
-
-    exploding_head = driver.find_element(By.XPATH, "//*[@id='reaction-butt-exploding_head']")
-    exploding_head.click()
-
-    raised_hands = driver.find_element(By.XPATH, "//*[@id='reaction-butt-raised_hands']")
-    raised_hands.click()
-
-    fire = driver.find_element(By.XPATH, "//*[@id='reaction-butt-fire']")
-    fire.click()
-
+    except Exception as e:
+        print(f"Error adding reaction or saving: {e}")
+    
 def sign_out(driver):
     try:
         driver.get("https://dev.to/signout_confirm")
@@ -57,6 +72,7 @@ def sign_out(driver):
 
 def handleStuff():
 
+    used_accouts = 0
     # chrome can be replaced with other browsers(Edge, Firefox)
     driver = webdriver.Chrome()
 
@@ -72,11 +88,17 @@ def handleStuff():
         try:
             sign_in(driver, username, password)
 
-            updoot(driver, url)
+            success = updoot(driver, url)
+            if(success):
+                used_accouts += 1
+
+            print(f"Used accounts: {used_accouts}/{len(users)}")
+            time.sleep(2.5)
 
             sign_out(driver)
 
             time.sleep(random.uniform(2, 5))
+
         except Exception as e:
             print(f"An error occurred with account {username}: {e}")
     
